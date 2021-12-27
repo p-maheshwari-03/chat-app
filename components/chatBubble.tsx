a85485de9a5f84614543c2dab61cf009c085471f
p-maheshwari-03 <pranav.maheshwari3.pm@gmail.com>
2021-12-27 20:13:59 +0530

    Update for bot messages distinguishable

--- components/chatBubble.tsx
+++ components/chatBubble.tsx
@@ -6,7 +6,7 @@ import { FC } from "react";
 const ChatBubble :FC<ChatBubbleType> = ( {message: { message, sender }, name }) => {
     return (
         <div className={styles.bubbleContainer}>
-            <div className={sender === name.trim() ? styles.userChatBubble : styles.nonUserChatBubble}>
+            <div className={sender === name.trim() ? styles.userChatBubble : sender == "Bot" ? styles.botChatBubble :styles.nonUserChatBubble}>
                 {message}
             </div>
             <span className={sender === name.trim() ? styles.username : styles.nonUsername}>{sender}</span>
